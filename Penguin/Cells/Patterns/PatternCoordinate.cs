ea23fbf48095d99de411f2e02bae177364ee9493
jcf120 <jossie.cliffordfrith@gmail.com>
2013-07-17 12:39:08 +0100

    Further simplified coordinate conversion.

--- Penguin/Cells/Patterns/PatternCoordinate.cs
+++ Penguin/Cells/Patterns/PatternCoordinate.cs
@@ -17,33 +17,8 @@ namespace Penguin
 		
 		public static PatternCoordinate operator+ (PatternCoordinate pc, CellVector cv)
 		{
-			/*int i = cv.i;
-			if ((i+pc.col)%2 != 0) i+=(i+pc.col)%2;
-			pc.row += i/2 + cv.j;
-			pc.col += cv.i;*/
-			/*if (pc.col%2==0) { // even column
-				if (cv.i<0) { // negative i
-					int i = cv.i%2;
-					pc.row += ((cv.i+i)/2) + cv.j;
-				} else { // positive i
-					int i = cv.i%2;
-					pc.row += ((cv.i-i)/2) + cv.j;
-				}
-			} else { // odd column
-				if (cv.i<0) { // negative i
-					int i = cv.i%2;
-					pc.row += ((cv.i-i)/2) + cv.j;
-				} else { // positive i
-					int i = cv.i%2;
-					pc.row += ((cv.i+i)/2) + cv.j;
-				}
-			}*/
 			int i = Math.Abs(cv.i%2);
-			if (pc.col%2==0) { // even column
-				pc.row += cv.j + ((cv.i-i)/2);
-			} else {           // odd column
-				pc.row += cv.j + ((cv.i+i)/2);
-			}
+			pc.row += cv.j + ((cv.i+((2*(pc.col%2)-1)*i))/2);
 			pc.col += cv.i;
 			return pc;
 		}
