2bf6d7d8befe3dbc0b5f5dd6b549931e91187983
jcf120 <jossie.cliffordfrith@gmail.com>
2013-07-21 15:42:55 +0100

    coorected perimeter test for new radius definition.

--- PenguinTest/CellMapTest.cs
+++ PenguinTest/CellMapTest.cs
@@ -28,7 +28,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[0], "Top right side cell "+i+" connected at index 0");
 				Assert.IsNull(c[1], "Top right side cell "+i+" connected at index 1");
-				if (i<r-1) c = c[2];
+				c = c[2];
 			}
 			
 			Assert.AreSame(cellMap.topRightCell, c, "Top Middle doesn't bridge to Top Right");
@@ -37,7 +37,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[1], "Right side cell "+i+" connected at index 1");
 				Assert.IsNull(c[2], "Right side cell "+i+" connected at index 2");
-				if (i<r-1) c = c[3];
+				c = c[3];
 			}
 			
 			Assert.AreSame(cellMap.bottomRightCell, c, "Top Right doesn't bridge to Bottom Right");
@@ -46,7 +46,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[2], "Bottom right side cell "+i+" connected at index 2");
 				Assert.IsNull(c[3], "Bottom right side cell "+i+" connected at index 3");
-				if (i<r-1) c = c[4];
+				c = c[4];
 			}
 			
 			Assert.AreSame(cellMap.bottomMiddleCell, c, "Bottom Right doesn't bridge to Bottom Middle");
@@ -55,7 +55,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[3], "Bottom left side cell "+i+" connected at index 3");
 				Assert.IsNull(c[4], "Bottom left side cell "+i+" connected at index 4");
-				if (i<r-1) c = c[5];
+				c = c[5];
 			}
 			
 			Assert.AreSame(cellMap.bottomLeftCell, c, "Bottom Middle doesn't bridge to Bottom Left");
@@ -64,7 +64,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[4], "Left side cell "+i+" connected at index 4");
 				Assert.IsNull(c[5], "Left side cell "+i+" connected at index 5");
-				if (i<r-1) c = c[0];
+				c = c[0];
 			}
 			
 			Assert.AreSame(cellMap.topLeftCell, c, "Bottom Left doesn't bridge to Top Right");
@@ -73,7 +73,7 @@ namespace PenguinTest
 			for (int i=0; i<r; i++) {
 				Assert.IsNull(c[5], "Top right side cell "+i+" connected at index 5");
 				Assert.IsNull(c[0], "Top right side cell "+i+" connected at index 0");
-				if (i<r-1) c = c[1];
+				c = c[1];
 			}
 			
 			Assert.AreSame(cellMap.topMiddleCell, c, "Top Left doesn't bridge to Top Middle");
