bb08b682cc76c5c1016630361efd198451609cd2
JCF120 <jossie.cliffordfrith@gmail.com>
2013-08-15 17:37:37 +0100

    Corrected store bounds checking. Store is now a SerializedProperty.

--- Penguin/Patterns/FreePattern.cs
+++ Penguin/Patterns/FreePattern.cs
@@ -6,13 +6,13 @@ namespace Penguin
 	public class FreePattern : CellPattern
 	{
 		public string storeName; // used by PatternArray controller to match store on unpacking
-		private FreePatternStore store_;
+		public FreePatternStore store;
 		
 		
 		public void OnEnable ()
 		{
-			if (store_ == null)
-				store_ = new FreePatternStore();
+			if (store == null)
+				store = new FreePatternStore();
 		}
 		
 		
@@ -20,8 +20,9 @@ namespace Penguin
 		{
 			// Convert to store's frame and check bounds
 			coor.col -= colsLeft;
-			if (coor.col < store_.width && coor.row < store_.height) {
-				return store_.values[coor.col,coor.row];
+			if (   coor.col >= 0 && coor.col < store.width
+				&& coor.row >= 0 && coor.row < store.height) {
+				return store.values[coor.col,coor.row];
 			}
 			else {
 				Debug.LogError("FreePattern queried beyond bounds. Defaulting to CellType.Empty");
