ed48f985e365484b19c9d26f179e8ee426d9c2b8
JCF120 <jossie.cliffordfrith@gmail.com>
2013-08-16 12:00:30 +0100

    Added FreePatternStoreLinker, which take a PatternArryController and a Dictionary of FreePatternStores and wires them up.

---
+++ Penguin/Patterns/FreePatternStoreLinker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Penguin;
+
+namespace Penguin
+{
+	public class FreePatternStoreLinker
+	{
+		public static void linkPacToStores(PatternArrayController pac, Dictionary<string, FreePatternStore> storesDict)
+		{
+			foreach (CellPattern pat in pac.patterns) {
+				
+				// Is it a FreePattern
+				if (pat.GetType() == typeof(FreePattern)) {
+					FreePattern freePat = pat as FreePattern;
+					
+					// Warn if already linked
+					if (freePat.store != null)
+						Debug.LogWarning("FreePattern's store '"+freePat.storeName+"' already linked - overwriting");
+					
+					// Does dict contain store?
+					if (storesDict.ContainsKey(freePat.storeName)) {
+						freePat.store = storesDict[freePat.storeName];
+					}
+					else {
+						Debug.LogError("FreePatternStoreLinker not passed store '"+freePat.storeName+"'.");
+					}
+				}
+			}
+		}
+	}
+}
+
